d3e623e13e5348ceb82f566234597f7feba81b09
TaigaMikami <mikamimikami60@gmail.com>
2022-07-04 01:28:29 +0900

    tailwindのprimaryTheme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
-import '../styles/globals.scss'
+import '@/styles/globals.scss'
+import '@/styles/prism.scss'
 import type { AppProps } from 'next/app'
 import LayoutWrapper from '@/components/LayoutWrapper'
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,59 +1,71 @@
-import Link from 'next/link'
+import Link from '@/components/Link'
 import Tag from '@/components/Tag'
 import { client } from '../libs/client'
 
 export default function Home({ blog }: { blog: any }) {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <ul>
-        {blog.map((blog: any) => {
-          const { id, title, content, eyecatch, category, date } = blog
-          return (
-            <li key={id} className="py-12">
-              <article>
-                <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
-                  <dl>
-                    <dt className="sr-only">Published on</dt>
-                    <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                      <time dateTime={date}>{date}</time>
-                    </dd>
-                  </dl>
-                  <div className="space-y-5 xl:col-span-3">
-                    <div className="space-y-6">
-                      <div>
-                        <h2 className="text-2xl font-bold leading-8 tracking-tight">
-                          <Link href={`/blogs/${id}`} className="text-gray-900 dark:text-gray-100">
-                            {title}
-                          </Link>
-                        </h2>
-                        {/* <div className="flex flex-wrap">
+    <>
+      <div className="divide-y divide-gray-200 dark:divide-gray-700">
+        <div className="space-y-2 pt-6 pb-8 md:space-y-5">
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            Latest
+          </h1>
+          {/* <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+            {siteMetadata.description}
+          </p> */}
+        </div>
+        <ul className="divide-y divide-gray-200 dark:divide-gray-700">
+          {blog.map((blog: any) => {
+            const { id, title, content, eyecatch, category, publishedAt } = blog
+            return (
+              <li key={id} className="py-12">
+                <article>
+                  <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
+                    <dl>
+                      <dt className="sr-only">Published on</dt>
+                      <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                        <time dateTime={publishedAt}>{publishedAt}</time>
+                      </dd>
+                    </dl>
+                    <div className="space-y-5 xl:col-span-3">
+                      <div className="space-y-6">
+                        <div>
+                          <h2 className="text-2xl font-bold leading-8 tracking-tight">
+                            <Link
+                              href={`/blogs/${id}`}
+                              className="text-gray-900 dark:text-gray-100"
+                            >
+                              {title}
+                            </Link>
+                          </h2>
+                          {/* <div className="flex flex-wrap">
                           {category.map((tag) => (
                             <Tag key={tag} text={tag} />
                           ))}
                         </div> */}
-                      </div>
-                      {/* <div className="prose max-w-none text-gray-500 dark:text-gray-400">
+                        </div>
+                        {/* <div className="prose max-w-none text-gray-500 dark:text-gray-400">
                         {summary}
                       </div> */}
-                    </div>
-                    <div className="text-base font-medium leading-6">
-                      <Link
-                        href={`/blogs/${id}`}
-                        className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-                        aria-label={`Read "${title}"`}
-                      >
-                        Read more &rarr;
-                      </Link>
+                      </div>
+                      <div className="text-base font-medium leading-6">
+                        <Link
+                          href={`/blogs/${id}`}
+                          className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                          aria-label={`Read "${title}"`}
+                        >
+                          Read more &rarr;
+                        </Link>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </article>
-            </li>
-          )
-        })}
-      </ul>
-    </div>
+                </article>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    </>
   )
 }
 
